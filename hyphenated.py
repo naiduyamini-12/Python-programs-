17a2572f7a85dfd8a059b74b56911ff784a8c62b
naiduyamini-12 <naiduyamini34@gmail.com>
2025-03-13 11:58:01 +0530

    Create hyphenated.py

---
+++ hyphenated.py
@@ -0,0 +1,12 @@
+Amount=650
+Five_Hundred_notes=Amount//500
+Remaining_balance=Amount%500
+Hundred_notes=Remaining_balance//100   
+Remaining_balance=Amount%100
+Fifty_notes=Remaining_balance//50
+Remaining_balance=Amount%50
+Ten_notes=Remaining_balance%10
+print("Five Hundred notes:",Five_Hundred_notes)
+print("Hundred notes:",Hundred_notes)
+print("Fifty Notes:",Fifty_notes)
+print("Ten Notes:",Ten_notes)
